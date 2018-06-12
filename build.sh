d39f24262d8be926af0c3980f8667135f0c10d24
Isaac <23095702+iBeizsley@users.noreply.github.com>
2018-06-12 14:34:24 +0100

    Revert "Added buildscript flag to only build and not package"
    This reverts commit 50e236d4346e630cade8f4c045535a28a6a280df.

--- build.sh
+++ build.sh
@@ -4,6 +4,4 @@ version="0.0.0"
 if [ -n "$1" ]; then version="$1"
 fi
 
-if [ "$2" == "true" ]; then dotnet build src/core/core.csproj
-else dotnet pack src/core/core.csproj -o ../../dist -p:Version="$version" -p:PackageVersion="$version" -c Release
-fi
+dotnet pack src/core/core.csproj -o ../../dist -p:Version="$version" -p:PackageVersion="$version" -c Release
