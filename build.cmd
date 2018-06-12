50e236d4346e630cade8f4c045535a28a6a280df
Isaac <23095702+iBeizsley@users.noreply.github.com>
2018-06-12 14:16:32 +0100

    Added buildscript flag to only build and not package

--- build.cmd
+++ build.cmd
@@ -3,4 +3,4 @@
 SET VERSION=0.0.0
 IF NOT [%1]==[] (set VERSION=%1)
 
-dotnet pack src/core/core.csproj -o ../../dist -p:Version="%VERSION%" -p:PackageVersion="%VERSION%" -c Release
+IF [%2]==[true] (dotnet build src/core/core.csproj) ELSE (dotnet pack src/core/core.csproj -o ../../dist -p:Version="%VERSION%" -p:PackageVersion="%VERSION%" -c Release)
--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ version="0.0.0"
 if [ -n "$1" ]; then version="$1"
 fi
 
-dotnet pack src/core/core.csproj -o ../../dist -p:Version="$version" -p:PackageVersion="$version" -c Release
+if [ "$2" == "true" ]; then dotnet build src/core/core.csproj
+else dotnet pack src/core/core.csproj -o ../../dist -p:Version="$version" -p:PackageVersion="$version" -c Release
+fi
